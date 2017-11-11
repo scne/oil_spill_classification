0f0d99d2717bb20facc5eb5c97b6a9407fccf35f
mariano <newton21890@gmail.com>
2017-11-11 19:19:39 +0100

    version 2.0

--- src/deep/deep_keras_conv.py
+++ src/deep/deep_keras_conv.py
@@ -1,98 +1,105 @@
-from keras.models import Model, Sequential # basic class for specifying and training a neural network
-from keras.layers import Input, Convolution2D, MaxPooling2D, Dense, Dropout, Activation, Flatten
-from keras.utils import np_utils # utilities for one-hot encoding of ground truth values
+#librerie standard
 import numpy as np
 import matplotlib.pyplot as plt
+#librerie keras
+from keras.models import Model
+from keras.layers import Input, Convolution2D, MaxPooling2D, Dense, Dropout, Flatten
 from keras.callbacks import TensorBoard, EarlyStopping
-
-from src.deep.create_dataset import _generate_dataset, _load_image, _extract_class, _plot_confusion_matrix
+from keras.models import load_model
+from keras.utils import np_utils
+#librerie sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
-from sklearn import preprocessing
+#funzioni
+from src.deep.create_dataset import _generate_dataset, _load_image, _plot_confusion_matrix
 
+#PATH PER IL SALVATAGGIO DEI DATI
+path_dataset = './deep/img_cluster' #PERCORSO DOVE SONO SALVATE LE IMMAGINI DI INPUT PER LA RETE
+path_model = './deep/model/model.h5'  #PERCORSO DOVE VIENE SALVATO IL MODELLO DOPO L'ADDESTRAMENTO DELLA RETE
 
-batch_size = 64# in each iteration, we consider 32 training examples at once
-num_epochs = 500 # we iterate 200 times over the entire training set
-kernel_size = 3 # we will use 3x3 kernels throughout
-pool_size = 2 # we will use 2x2 pooling throughout
-conv_depth_1 = 32 # we will initially have 32 kernels per conv. layer...
-conv_depth_2 = 64 # ...switching to 64 after the first pooling layer
-drop_prob_1 = 0.25 # dropout after pooling with probability 0.25
-drop_prob_2 = 0.5 # dropout in the FC layer with probability 0.5
-hidden_size = 512 # the FC layer will have 512 neurons
+#PARAMETRI PER LA CONVOLUTIONAL DEEP NEURAL NETWORK
+batch_size = 64 #quantità di trainig cases elaborati per esecuzione
+num_epochs = 500 #numero massimo di epoche per la quale la rete può essere addestrata
+kernel_size = 3 #dimensione del kernel per l'operazione di convolusione della rete
+pool_size = 2 #dimensione dello strato di pooling per la rete
+seed = 42 #seme per la generazione random dei pesi
 
-
-filenames, labels = _generate_dataset('./img_autoe')
-
-
-x_train, x_test, y_train, y_test = train_test_split(filenames, labels, test_size=0.30, random_state=42, stratify=labels)
-
-X_train = np.asarray(_load_image(x_train))
-X_test = np.asarray(_load_image(x_test))
-Y_train = y_train
-Y_test = y_test
-
-
+#PARAMETRI DIMENSIONE IMMAGINI
 height = 32
 width = 32
 depth = 1
-num_train = len(X_train)
-# num_train, height, width, depth = X_train.shape # there are 50000 training examples in CIFAR-10
-num_test = len(X_test)
-# num_test = X_test.shape[0] # there are 10000 test examples in CIFAR-10
-num_classes = np.unique(y_train).shape[0] # there are 10 image classes
 
 
-print("ciaoooo")
-# Y_train = _one_hot_label(y_train)
-Y_train = np_utils.to_categorical(y_train, num_classes) # One-hot encode the labels
-Y_test = np_utils.to_categorical(y_test, num_classes) # One-hot encode the labels
+def _load_data_cnn():
 
-inp = Input(shape=(height, width, depth))
-conv_1 = Convolution2D(32, (kernel_size, kernel_size), padding='same', activation='relu')(inp)
-pool_1 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_1)
-conv_2 = Convolution2D(64, (kernel_size, kernel_size), padding='same', activation='relu')(pool_1)
-pool_2 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_2)
-drop_1 = Dropout(0.25)(pool_2)
-flat = Flatten()(drop_1)
-# hidden_1 = Dense(4096, activation='relu')(flat)
-hidden_2 = Dense(1024, activation='relu')(flat)
-drop_2 = Dropout(0.4)(hidden_2)
-out = Dense(3, activation='softmax')(drop_2)
+    filenames, labels = _generate_dataset(path_dataset)
+    x_train, x_test, y_train, y_test = train_test_split(filenames, labels, test_size=0.30, random_state=seed, stratify=labels)
+    X_train = np.asarray(_load_image(x_train))
+    X_test = np.asarray(_load_image(x_test))
+    num_classes = np.unique(y_train).shape[0]
+    Y_train = np_utils.to_categorical(y_train, num_classes)
+    Y_test = np_utils.to_categorical(y_test, num_classes)
 
-model = Model(inputs=inp, outputs=out) # To define a model, just specify its input and output layers
-model.compile(optimizer='adadelta',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+    return X_train, X_test, Y_train, Y_test
 
-print(model.summary())
+def _evaluete_cnn(X_train, X_test, Y_train, Y_test):
 
-model.fit(X_train, Y_train, batch_size=batch_size, epochs=num_epochs, verbose=1, validation_split=0.1,
-          callbacks=[TensorBoard(log_dir='./board2'), EarlyStopping(monitor='loss', min_delta=0.1, patience=10,
-                                                                    verbose=0, mode='auto')])
+    model = load_model(path_model)
 
-model.evaluate(X_test, Y_test, verbose=1, sample_weight=None)  # Evaluate the trained model on the test set!
+    model.evaluate(X_test, Y_test, verbose=1, sample_weight=None)  # Evaluate the trained model on the test set!
 
-print("Saving model")
-model.save("./model/model.h5", overwrite= True)
+    score = model.evaluate(X_test, Y_test, verbose=1)  # Evaluate the trained model on the test set!
 
-score = model.evaluate(X_test, Y_test, verbose=1)  # Evaluate the trained model on the test set!
+    y_predict = np.asarray(model.predict(X_test, verbose=0))
+    Y_predict = np.argmax(y_predict, axis=1)
+    y_test = np.argmax(Y_test, axis=1)
+    confmatrix = confusion_matrix(y_test, Y_predict)
+    print("\n Confusion Matrix :")
+    print(confmatrix)
+    class_names = ["0", "1", '2']
+    plt.figure()
+    # _plot_confusion_matrix(confmatrix, classes=class_names, title='Confusion matrix')
 
-y_predict = np.asarray(model.predict(X_test, verbose=0))
-Y_predict = np.argmax(y_predict, axis=1)
-confmatrix = confusion_matrix(y_test, Y_predict)
-print("\n Confusion Matrix :")
-# print(confmatrix)
-
-class_names = ["0", "1", '2']
-plt.figure()
-_plot_confusion_matrix(confmatrix, classes=class_names, title='Confusion matrix')
+    print("\nMetrics => ", model.metrics_names, score)
+    print('\nClassification Report : ')
+    print(classification_report(y_test, Y_predict, target_names=class_names))
+    # plt.show()
 
 
-print("\nMetrics => ")
-print(model.metrics_names)
-print(score)
-print('\nClassification Report : ')
-print(classification_report(y_test, Y_predict, target_names=class_names))
-# plt.show()
-print("The End")
+
+def _start_cnn ():
+    print('STARTING FITTING CONVOLUTIONAL DEEP NEURAL NETWORK')
+    X_train, X_test, Y_train, Y_test = _load_data_cnn()
+    print('loading data .......')
+    #DEFINIZIONE DEL MODELLO DELLA CONVOLUTIONAL DEEP NEURAL NETWORK
+    inp = Input(shape=(height, width, depth))
+    conv_1 = Convolution2D(32, (kernel_size, kernel_size), padding='same', activation='relu')(inp)
+    pool_1 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_1)
+    conv_2 = Convolution2D(64, (kernel_size, kernel_size), padding='same', activation='relu')(pool_1)
+    pool_2 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_2)
+    drop_1 = Dropout(0.25)(pool_2)
+    flat = Flatten()(drop_1)
+    # hidden_1 = Dense(4096, activation='relu')(flat)
+    hidden_2 = Dense(1024, activation='relu')(flat)
+    drop_2 = Dropout(0.4)(hidden_2)
+    out = Dense(3, activation='softmax')(drop_2)
+
+    #CREAZIONE DEL MODELLO E COMPILAZIONE
+    model = Model(inputs=inp, outputs=out) # To define a model, just specify its input and output layers
+    model.compile(optimizer='adadelta',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    # print(model.summary())
+    #STARTING FITTING
+    model.fit(X_train, Y_train, batch_size=batch_size, epochs=num_epochs, verbose=1, validation_split=0.1,
+              callbacks=[TensorBoard(log_dir='./deep/tensorboard/'),
+                         EarlyStopping(monitor='loss', min_delta=0.1, patience=10,
+                                       verbose=0, mode='auto')])
+
+    #SALVO IL MODELLO APPENA ADDESTRATO
+    print("Saving model")
+    model.save(path_model, overwrite=True)
+    #PASSO ALLA FASE DI VALUTAZIONE DEL MODELLO SUI TEST CASES
+    _evaluete_cnn(X_train, X_test, Y_train, Y_test)
+    print("The End")
